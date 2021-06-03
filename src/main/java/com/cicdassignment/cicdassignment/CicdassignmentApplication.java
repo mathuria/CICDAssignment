d9c82959a26bd4a950271668883cfd9f91a35568
Aishwarya Mathuria <aishwaryamathuria1992@gmail.com>
2021-06-03 12:46:25 +0000

    first commit

---
+++ src/main/java/com/cicdassignment/cicdassignment/CicdassignmentApplication.java
@@ -0,0 +1,13 @@
+package com.cicdassignment.cicdassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdassignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdassignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cicdassignment/cicdassignment/CicdassignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicdassignment.cicdassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdassignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
